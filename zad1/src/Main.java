e10b0da6aa74d11df1c3cf8026dd3cd22a1c4ad1
olekkosma <akosma@sigma.ug.edu.pl>
2017-10-13 23:04:27 +0200

    poprawki w kodzie, finish opracowania

--- zad1/src/Main.java
+++ zad1/src/Main.java
@@ -2,44 +2,26 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.lang.StrictMath;
 import java.util.Collections;
 
 public class Main {
-
-
     public static void main(String[] args){
 
-        //TODO zrobienie wyników dla paru tysiecy wejsciowych
-        //TODO usrednienie i wykazanie poprzez wartosc bezwzgledna ze od tylu jest efektywniej : )
-        //TODO wyjasnienie czemu to wynika, oczywiscie z przesuwania floating point
-        //TODO sprawdzenie czy nasz algorytm oblicza precyzyjniej  lepiej niz biblioteka
-        //TODO poprzez sprawdzeeni 16 miejsca po przecinku czy sa takie same zawsze w zaokragleniu...ehh
-        //TODO odpowiednie wykresy tzn... np zbieganie danej funkcji od tylu i przodu do naszego wyniku wzdluz iteracji sumy..
-        //TODO po chuj nie wiem takie cos xD
-        //TODO czemu dla normalnego wzoru wynik potem jest zły
-        //TODO chyba dlatego ze kurwa licze na doublach wynik raczej nie xD ja jebie, konwersja przy inicializacji
-        //TODO z gownianego powodu do 1 tylko mozna sumowac potem sie pierdoli chuj wie czemu
-
-
-
-
-        //TODO wnioski:
-        //TODO sumowanie ze wzoru nie działa potem bo licze na ddoublach w momencie liczenia ułamka-pattern
-        //TODO moj big decimal liczy najprecyzejniej : D
         ArrayList<BigDecimal> bigDecimalList = new ArrayList<>();
-        MathContext mc = new MathContext(32, RoundingMode.HALF_UP);
-        double valueDouble = 0.6;
+        MathContext mc = new MathContext(32, RoundingMode.HALF_DOWN);
+        double valueDouble = 0.2137;
         System.out.print("Output of Math libary:            ");
         System.out.println(Math.atan(valueDouble));
-        System.out.print("Output of wolfram:                0.54041950027058415544357836460859\n");
+        //System.out.print("Output of wolfram:                0.54041950027058415544357836460859\n");
 
                     //Sumowanie ze szeregu MacLourina
             //BigDecimal
         //Od przodu
         //( (-1)^n * x^(2n +1) )/ (2n+1)
 
-        BigDecimal value = BigDecimal.valueOf(valueDouble);
-        BigDecimal sum = new BigDecimal(0);
+        BigDecimal value;
+        BigDecimal sum;
         BigDecimal pattern;
         int n=0;
         do{
@@ -52,7 +34,7 @@ public class Main {
 
             bigDecimalList.add(pattern);
             n++;
-        }while(n!=50);
+        }while(n!=350);
 
         BigDecimal sumForward = new BigDecimal(0);
         for(BigDecimal listValue : bigDecimalList){
@@ -60,8 +42,7 @@ public class Main {
 
         }
         System.out.print("Output of suming values forward:  ");
-        System.out.println(sumForward.setScale(32, RoundingMode.HALF_EVEN).toPlainString());
-
+        System.out.println(sumForward.setScale(17, RoundingMode.HALF_EVEN).toPlainString());
 
         //od tyłu
 
@@ -73,7 +54,7 @@ public class Main {
 
         }
         System.out.print("Output of suming values backward: ");
-        System.out.println(sumBackward.setScale(32, RoundingMode.HALF_EVEN).toPlainString());
+        System.out.println(sumBackward.setScale(17, RoundingMode.HALF_DOWN).toPlainString());
 
             //double
         //od prozdu
@@ -85,7 +66,7 @@ public class Main {
             patternDouble = Math.pow((-1),n)*Math.pow(valueDouble,(2*n +1))/(2*n +1);
             doubleList.add(patternDouble);
             n++;
-        }while(n!=27);
+        }while(n!=350);
 
         double sumForwardDouble = 0;
         for(Double doubleValue : doubleList){
@@ -97,7 +78,6 @@ public class Main {
 
         //od tyłu
 
-
         double sumBackwardDouble = 0;
         ArrayList<Double> doubleListInvert = invertListDouble(doubleList);
         for(Double doubleValue : doubleListInvert){
@@ -107,12 +87,10 @@ public class Main {
         System.out.print("Output of suming double backward: ");
         System.out.println(sumBackwardDouble);
 
-
                 //sumowanie na podstawie poprzedniego
             //BigDecimal
         //Od przodu
         //((-(value*value))*(2*n+1))/(2*n+3);
-
 
         value = new BigDecimal(valueDouble);
         sum = new BigDecimal(valueDouble);
@@ -124,24 +102,20 @@ public class Main {
             tmp = tmp.multiply(tmp);
             tmp = tmp.negate();
             tmp = tmp.multiply(BigDecimal.valueOf(2*n + 1));
-
             tmp = tmp.divide(BigDecimal.valueOf((2*n + 3)),mc);
-
             nextElement = nextElement.multiply(tmp);
             sum = sum.add(nextElement);
-
             n++;
-        }while(n != 500);
+        }while(n != 350);
 
         System.out.print("Output of suming next forward:    ");
-        System.out.println(sum.setScale(32, RoundingMode.HALF_DOWN).toPlainString());
+        System.out.println(sum.setScale(17, RoundingMode.HALF_DOWN).toPlainString());
 
         //od tyłu
 
         bigDecimalList.clear();
         bigDecimalListInvert.clear();
         nextElement = value;
-        value = new BigDecimal(valueDouble);
         sum = new BigDecimal(valueDouble);
         n=0;
 
@@ -150,15 +124,12 @@ public class Main {
             tmp = tmp.multiply(tmp);
             tmp = tmp.negate();
             tmp = tmp.multiply(BigDecimal.valueOf(2*n + 1));
-
             tmp = tmp.divide(BigDecimal.valueOf((2*n + 3)),mc);
-
             nextElement = nextElement.multiply(tmp);
             bigDecimalList.add(nextElement);
-            //sum = sum.add(nextElement);
 
             n++;
-        }while(n != 500);
+        }while(n != 350);
 
         bigDecimalListInvert = invertList(bigDecimalList);
 
@@ -168,7 +139,7 @@ public class Main {
         }
 
         System.out.print("Output of suming next backward:   ");
-        System.out.println(sum.setScale(32, RoundingMode.HALF_DOWN).toPlainString());
+        System.out.println(sum.setScale(17, RoundingMode.HALF_DOWN).toPlainString());
 
             //double
         //od prozdu
@@ -182,15 +153,14 @@ public class Main {
         doubleList.clear();
 
         do{
-            Double tmp = valueDouble;
-           tmp = ((-(valueDouble*valueDouble))*(2*n+1))/(2*n+3);
-
+            Double tmp;
+            tmp = ((-(valueDouble*valueDouble))*(2*n+1))/(2*n+3);
             nextElementDouble = nextElementDouble * tmp;
             sumDouble = sumDouble +nextElementDouble;
             doubleList.add(nextElementDouble);
             n++;
 
-        }while(n != 500);
+        }while(n != 350);
 
         System.out.print("Output of double next forward:    ");
         System.out.println(sumDouble);
@@ -207,11 +177,11 @@ public class Main {
         sumBackwardDouble+=valueDouble;
         System.out.print("Output of double next backward:   ");
         System.out.println(sumBackwardDouble);
+        /*
 
-/*
 //                              LICZENIE KTÓRE SUMOWANIE JEST PRECYZYJNIEJSZE
 
-        int repetition = 1000;
+        int repetition = 100000;
         double valueChanged= 0.9;
         double libaryResult;
         double forwardClosed = 0;
@@ -219,7 +189,7 @@ public class Main {
         double backwardClosedAverage = 0;
         double forwardClosedAverage = 0;
         for(int i=0;i<repetition;i++){
-            valueChanged+=0.0001;
+            valueChanged+=0.000001;
             libaryResult  = Math.atan(valueChanged);
             ArrayList<Double> list = atanArrayDouble(valueChanged);
             double resultForward = atanDoubleForward(list);
@@ -228,11 +198,11 @@ public class Main {
             double diffBackward = Math.abs(libaryResult-resultBackward);
             System.out.println();
             System.out.printf("%.17f\n",valueChanged);
-            System.out.println("libary result:   "+libaryResult);
-            System.out.println("forward result:  "+resultForward);
-            System.out.println("backward result: "+resultBackward);
-            System.out.printf("diff forward:    %.17f\n",diffForward);
-            System.out.printf("diff bacward:    %.17f\n",diffBackward);
+           // System.out.println("libary result:   "+libaryResult);
+           // System.out.println("forward result:  "+resultForward);
+            //System.out.println("backward result: "+resultBackward);
+            //System.out.printf("diff forward:    %.17f\n",diffForward);
+            //System.out.printf("diff bacward:    %.17f\n",diffBackward);
             backwardClosedAverage+=diffBackward;
             forwardClosedAverage+=diffForward;
 
