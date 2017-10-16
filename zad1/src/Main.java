5a05be0d2c75973198312adcc828df13be72bf9d
olekkosma <akosma@sigma.ug.edu.pl>
2017-10-16 16:45:32 +0200

    spora edycja kodu i pdf

--- zad1/src/Main.java
+++ zad1/src/Main.java
@@ -6,45 +6,38 @@ import java.lang.StrictMath;
 import java.util.Collections;
 
 public class Main {
-    public static void main(String[] args){
 
+    static int iterations = 300;
+
+    public static ArrayList<BigDecimal> ValuesOfMcLourinList(String value) {
+
+        MathContext mc = new MathContext(80, RoundingMode.HALF_DOWN);
         ArrayList<BigDecimal> bigDecimalList = new ArrayList<>();
-        MathContext mc = new MathContext(320, RoundingMode.HALF_DOWN);
-        double valueDouble = 0.2222;
-        System.out.print("Output of Math libary:            ");
-        System.out.println(Math.atan(valueDouble));
-        //System.out.print("Output of wolfram:                0.54041950027058415544357836460859\n");
 
-                    //Sumowanie ze szeregu MacLourina
-            //BigDecimal
-        //Od przodu
-        //( (-1)^n * x^(2n +1) )/ (2n+1)
-
-        BigDecimal value;
-        BigDecimal sum;
-        BigDecimal pattern;
-        int n=0;
-        do{
-            BigDecimal tmp = new BigDecimal("0.2222");
-            tmp = tmp.pow(2*n+1);
-            tmp = tmp.divide(BigDecimal.valueOf((2*n+1)),mc);
-            pattern = new BigDecimal(Math.pow((-1),n));
+        int n = 0;
+        do {
+            BigDecimal tmp = new BigDecimal(value);
+            tmp = tmp.pow(2 * n + 1);
+            tmp = tmp.divide(BigDecimal.valueOf((2 * n + 1)), mc);
+            BigDecimal pattern = new BigDecimal(Math.pow((-1), n));
             pattern = pattern.multiply(tmp);
-            //pattern = new BigDecimal(Math.pow((-1),n)*Math.pow((valueDouble),(2*n +1))/(2*n +1));
-
             bigDecimalList.add(pattern);
             n++;
-        }while(n!=350);
 
+        } while (n != iterations);
+
+        return bigDecimalList;
+    }
+    public static BigDecimal sumForwardList(ArrayList<BigDecimal> bigDecimalList){
         BigDecimal sumForward = new BigDecimal(0);
         for(BigDecimal listValue : bigDecimalList){
             sumForward = sumForward.add(listValue);
 
         }
-        System.out.print("Output of suming values forward:  ");
-        System.out.println(sumForward.setScale(64, RoundingMode.HALF_EVEN).toPlainString());
+        return sumForward;
+    }
 
-        //od tyłu
+    public static BigDecimal sumBackwardList(ArrayList<BigDecimal> bigDecimalList){
 
         ArrayList<BigDecimal> bigDecimalListInvert = invertList(bigDecimalList);
         BigDecimal sumBackward = new BigDecimal(0);
@@ -53,30 +46,39 @@ public class Main {
             sumBackward = sumBackward.add(listValue);
 
         }
-        System.out.print("Output of suming values backward: ");
-        System.out.println(sumBackward.setScale(64, RoundingMode.HALF_DOWN).toPlainString());
+        return sumBackward;
+    }
 
-            //double
-        //od prozdu
-        //( (-1)^n * x^(2n +1) )/ (2n+1)
+
+
+    public static ArrayList<Double> ValuesOfMcLourinListDouble(String value) {
+
         ArrayList<Double> doubleList = new ArrayList<>();
         double patternDouble;
-        n=0;
-        do{
-            patternDouble = Math.pow((-1),n)*Math.pow(valueDouble,(2*n +1))/(2*n +1);
+        double valueDouble = Double.parseDouble(value);
+        int n = 0;
+        do {
+            patternDouble = Math.pow((-1), n) * Math.pow(valueDouble, (2 * n + 1)) / (2 * n + 1);
             doubleList.add(patternDouble);
             n++;
-        }while(n!=1350);
+        } while (n != iterations);
+
+        return doubleList;
+    }
+
+    public static Double sumForwardListDouble(ArrayList<Double> doubleList){
 
         double sumForwardDouble = 0;
         for(Double doubleValue : doubleList){
             sumForwardDouble = sumForwardDouble+doubleValue;
 
         }
-        System.out.print("Output of suming double forward:  ");
-        System.out.println(sumForwardDouble);
+       return sumForwardDouble;
 
-        //od tyłu
+
+    }
+
+    public static Double sumBackwardListDouble(ArrayList<Double> doubleList){
 
         double sumBackwardDouble = 0;
         ArrayList<Double> doubleListInvert = invertListDouble(doubleList);
@@ -84,74 +86,40 @@ public class Main {
             sumBackwardDouble = sumBackwardDouble+doubleValue;
 
         }
-        System.out.print("Output of suming double backward: ");
-        System.out.println(sumBackwardDouble);
+       return sumBackwardDouble;
 
-                //sumowanie na podstawie poprzedniego
-            //BigDecimal
-        //Od przodu
-        //((-(value*value))*(2*n+1))/(2*n+3);
+    }
 
-        value = new BigDecimal("0.2222");
-        sum = new BigDecimal("0.2222");
-        n=0;
-        BigDecimal  nextElement = value;
+    public static ArrayList<BigDecimal> ValuesOfCountNextList(String value) {
 
+        MathContext mc = new MathContext(70, RoundingMode.HALF_DOWN);
+        ArrayList<BigDecimal> bigDecimalList = new ArrayList<>();
+        BigDecimal  nextElement = new BigDecimal(value);
+        bigDecimalList.add(new BigDecimal(value));
+        int n=0;
         do{
-            BigDecimal tmp = value;
-            tmp = tmp.multiply(tmp);
-            tmp = tmp.negate();
-            tmp = tmp.multiply(BigDecimal.valueOf(2*n + 1));
-            tmp = tmp.divide(BigDecimal.valueOf((2*n + 3)),mc);
-            nextElement = nextElement.multiply(tmp);
-            sum = sum.add(nextElement);
-            n++;
-        }while(n != 350);
-
-        System.out.print("Output of suming next forward:    ");
-        System.out.println(sum.setScale(64, RoundingMode.HALF_DOWN).toPlainString());
-
-        //od tyłu
-
-        bigDecimalList.clear();
-        bigDecimalListInvert.clear();
-        nextElement = value;
-        sum = new BigDecimal(valueDouble);
-        n=0;
-
-        do{
-            BigDecimal tmp = value;
+            BigDecimal tmp =  new BigDecimal(value);
             tmp = tmp.multiply(tmp);
             tmp = tmp.negate();
             tmp = tmp.multiply(BigDecimal.valueOf(2*n + 1));
             tmp = tmp.divide(BigDecimal.valueOf((2*n + 3)),mc);
             nextElement = nextElement.multiply(tmp);
             bigDecimalList.add(nextElement);
-
             n++;
-        }while(n != 350);
+        }while(n != iterations);
 
-        bigDecimalListInvert = invertList(bigDecimalList);
+       return bigDecimalList;
+    }
 
-        for(BigDecimal listValue : bigDecimalListInvert){
-            sum = sum.add(listValue);
+    public static ArrayList<Double> ValuesOfCountNextListDouble(String value) {
 
-        }
+        Double  nextElementDouble = Double.parseDouble(value);
+        ArrayList<Double> doubleList = new ArrayList<>();
+        double valueDouble = Double.parseDouble(value);
+        doubleList.add(valueDouble);
+        Double sumDouble = Double.parseDouble(value);
 
-        System.out.print("Output of suming next backward:   ");
-        System.out.println(sum.setScale(64, RoundingMode.HALF_DOWN).toPlainString());
-
-            //double
-        //od prozdu
-
-        //((-(value*value))*(2*n+1))/(2*n+3);
-
-        Double sumDouble = valueDouble;
-        n=0;
-        Double  nextElementDouble = valueDouble;
-
-        doubleList.clear();
-
+        int n=0;
         do{
             Double tmp;
             tmp = ((-(valueDouble*valueDouble))*(2*n+1))/(2*n+3);
@@ -160,49 +128,108 @@ public class Main {
             doubleList.add(nextElementDouble);
             n++;
 
-        }while(n != 350);
+        }while(n != iterations);
+        return doubleList;
+    }
 
-        System.out.print("Output of double next forward:    ");
-        System.out.println(sumDouble);
+    public static ArrayList<BigDecimal> invertList(ArrayList<BigDecimal> list){
+        ArrayList<BigDecimal> toReturn = new ArrayList<>();
+
+        for(int i = list.size()-1 ; i>=0 ; i--){
+            toReturn.add(list.get(i));
+        }
+        return toReturn;
+    }
+
+    public static ArrayList<Double> invertListDouble(ArrayList<Double> list){
+        ArrayList<Double> toReturn = new ArrayList<>();
+
+        for(int i = list.size()-1 ; i>=0 ; i--){
+            toReturn.add(list.get(i));
+        }
+        return toReturn;
+    }
+
+    public static void countOwnArctanValues(String value){
+
+        System.out.print("Output of Math libary:              ");
+        System.out.println(Math.atan(Double.parseDouble(value))+"");
+        //System.out.print("Output of Wolfram:                  ");
+        //System.out.println("0.2303949468807240960351954018340492626054069551810387153331776640\n");
+
+        //Sumowanie ze szeregu MacLourina
+        //BigDecimal
+        //Od przodu
+        //( (-1)^n * x^(2n +1) )/ (2n+1)
+
+        System.out.print("Sum forward McLourin BigDecimal:    ");
+        System.out.println(sumForwardList(ValuesOfMcLourinList(value))
+                .setScale(64, RoundingMode.HALF_UP).toPlainString());
 
         //od tyłu
 
-        doubleListInvert = invertListDouble(doubleList);
-        sumBackwardDouble = 0;
+        System.out.print("Sum backward McLourin BigDecimal:   ");
+        System.out.println(sumBackwardList(ValuesOfMcLourinList(value))
+                .setScale(64, RoundingMode.HALF_UP).toPlainString());
 
-        for(Double doubleValue : doubleListInvert){
-            sumBackwardDouble = sumBackwardDouble+doubleValue;
+        //double
+        //od prozdu
+        //( (-1)^n * x^(2n +1) )/ (2n+1)
 
-        }
-        sumBackwardDouble+=valueDouble;
-        System.out.print("Output of double next backward:   ");
-        System.out.println(sumBackwardDouble);
+        System.out.print("\nSum forward McLourin Double:        ");
+        System.out.println(sumForwardListDouble(ValuesOfMcLourinListDouble(value)));
 
-/*
-//                              LICZENIE KTÓRE SUMOWANIE JEST PRECYZYJNIEJSZE
+        //od tyłu
 
-        int repetition = 100000;
-        double valueChanged= 0.9;
+        System.out.print("Sum backward McLourin Double:       ");
+        System.out.println(sumBackwardListDouble(ValuesOfMcLourinListDouble(value)));
+
+        //sumowanie na podstawie poprzedniego
+        //BigDecimal
+        //Od przodu
+        //((-(value*value))*(2*n+1))/(2*n+3);
+
+        System.out.print("\nSum forward next BigDecimal:        ");
+        System.out.println(sumForwardList(ValuesOfCountNextList(value))
+                .setScale(64, RoundingMode.HALF_UP).toPlainString());
+
+        //od tyłu
+
+        System.out.print("Sum backward next BigDecimal:       ");
+        System.out.println(sumBackwardList(ValuesOfCountNextList(value))
+                .setScale(64, RoundingMode.HALF_UP).toPlainString());
+        //double
+        //od prozdu
+        //((-(value*value))*(2*n+1))/(2*n+3);
+
+        System.out.print("\nSum forward next Double:            ");
+        System.out.println(sumForwardListDouble(ValuesOfCountNextListDouble(value)));
+
+        //od tyłu
+
+        System.out.print("Sum backward next Double:           ");
+        System.out.println(sumBackwardListDouble(ValuesOfCountNextListDouble(value)));
+
+    }
+
+    public static void CountStatisticsOfArc(Double startValue,Double endValue,int samples){
+
         double libaryResult;
         double forwardClosed = 0;
         double backwardClosed = 0;
         double backwardClosedAverage = 0;
         double forwardClosedAverage = 0;
-        for(int i=0;i<repetition;i++){
-            valueChanged+=0.000001;
-            libaryResult  = Math.atan(valueChanged);
-            ArrayList<Double> list = atanArrayDouble(valueChanged);
-            double resultForward = atanDoubleForward(list);
-            double resultBackward = atanDoubleBackward(list);
+        Double step = (endValue-startValue)/samples;
+
+        for(int i=0;i<samples;i++){
+
+            startValue+=step;
+            libaryResult  = Math.atan(startValue);
+            ArrayList<Double> list = ValuesOfMcLourinListDouble(String.valueOf(startValue));
+            double resultForward = sumForwardListDouble(list);
+            double resultBackward = sumBackwardListDouble(list);
             double diffForward = Math.abs(libaryResult-resultForward);
             double diffBackward = Math.abs(libaryResult-resultBackward);
-            System.out.println();
-            System.out.printf("%.17f\n",valueChanged);
-           // System.out.println("libary result:   "+libaryResult);
-           // System.out.println("forward result:  "+resultForward);
-            //System.out.println("backward result: "+resultBackward);
-            //System.out.printf("diff forward:    %.17f\n",diffForward);
-            //System.out.printf("diff bacward:    %.17f\n",diffBackward);
             backwardClosedAverage+=diffBackward;
             forwardClosedAverage+=diffForward;
 
@@ -213,69 +240,26 @@ public class Main {
                     forwardClosed++;
                 }
             }
-
+            //System.out.println();
+            //System.out.printf("%.17f\n",startValue);
+            // System.out.println("libary result:   "+libaryResult);
+            // System.out.println("forward result:  "+resultForward);
+            //System.out.println("backward result: "+resultBackward);
+            //System.out.printf("diff forward:    %.17f\n",diffForward);
+            //System.out.printf("diff bacward:    %.17f\n",diffBackward);
         }
-
-        System.out.println("backward is better in: "+backwardClosed/repetition * 100+" %");
-        System.out.printf("average diffrence: %.22f\n",backwardClosedAverage/repetition);
-        System.out.println("forward is better in: "+forwardClosed/repetition* 100+" %");
-        System.out.printf("average diffrence: %.22f\n",forwardClosedAverage/repetition);
-
-*/
-    }
-
-    public static  ArrayList<Double> atanArrayDouble(double value){
-        ArrayList<Double> doubleList = new ArrayList<>();
-        double patternDouble;
-        int n=0;
-        do{
-            patternDouble = Math.pow((-1),n)*Math.pow(value,(2*n +1))/(2*n +1);
-            doubleList.add(patternDouble);
-            n++;
-        }while(n!=25);
-
-        return doubleList;
-
+        System.out.println("backward is better in: "+backwardClosed/samples * 100+" %");
+        System.out.printf("average diffrence: %.22f\n",backwardClosedAverage/samples);
+        System.out.println("forward is better in: "+forwardClosed/samples* 100+" %");
+        System.out.printf("average diffrence: %.22f\n",forwardClosedAverage/samples);
 
     }
-    public static  double atanDoubleForward(ArrayList<Double> doubleList){
 
-        double sumForwardDouble = 0;
-        for(Double doubleValue : doubleList){
-            sumForwardDouble = sumForwardDouble+doubleValue;
+    public static void main(String[] args){
 
-        }
+        countOwnArctanValues("0.23456");
 
-        return sumForwardDouble;
+        //CountStatisticsOfArc(0.9,1.0,100000);
+
     }
-    public static double atanDoubleBackward(ArrayList<Double> doubleList){
-
-        ArrayList<Double> doubleListInvert = invertListDouble(doubleList);
-
-
-        return atanDoubleForward(doubleListInvert);
-    }
-
-    public static ArrayList<BigDecimal> invertList(ArrayList<BigDecimal> list){
-        ArrayList<BigDecimal> toReturn = new ArrayList<>();
-
-        for(int i = list.size()-1 ; i>=0 ; i--){
-            toReturn.add(list.get(i));
-        }
-
-
-        return toReturn;
-    }
-
-    public static ArrayList<Double> invertListDouble(ArrayList<Double> list){
-        ArrayList<Double> toReturn = new ArrayList<>();
-
-        for(int i = list.size()-1 ; i>=0 ; i--){
-            toReturn.add(list.get(i));
-        }
-
-
-        return toReturn;
-    }
-
 }
