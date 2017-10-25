5fa6eca69d9aa71f8b35d0c6a5a52d3c7ca90a39
olekkosma <akosma@sigma.ug.edu.pl>
2017-10-26 00:25:05 +0200

    first generic class test made...

--- zad2/Matrix/src/GenericValue.java
+++ zad2/Matrix/src/GenericValue.java
@@ -1,12 +1,26 @@
 public class GenericValue<T extends Number> {
-    T value;
+    String value;
     final Class<T> classType;
 
-    public GenericValue(Class<T> classType,T value){
+
+    public GenericValue(Class<T> classType,String value){
         this.classType = classType;
         this.value = value;
     }
+
+    public <T extends Number> T convertion(){
+
+        if(classType.equals(Double.class)){
+            return (T) Double.valueOf(value);
+        }
+        if(classType.equals(Float.class)){
+            return (T) Float.valueOf(value);
+        }
+        return null;
+    }
+
+
     public void printValue(){
-        System.out.println(this.value);
+        System.out.println(convertion());
     }
 }
